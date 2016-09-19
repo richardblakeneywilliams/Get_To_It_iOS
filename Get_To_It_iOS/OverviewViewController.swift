00c002956ae80c768742810349f9b1b8fb4db3b4
Richard <richardbw777@live.com>
2016-09-19 19:47:48 +1200

    I have the table view in there. Might change it to just UILabels with borders.

--- Get_To_It_iOS/OverviewViewController.swift
+++ Get_To_It_iOS/OverviewViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 import XLPagerTabStrip
 
-class OverviewViewController: UIViewController, IndicatorInfoProvider, UICollectionViewDelegate, UICollectionViewDataSource {
+class OverviewViewController: UIViewController, IndicatorInfoProvider, UICollectionViewDelegate, UICollectionViewDataSource, UITableViewDelegate, UITableViewDataSource {
     
     
+    @IBOutlet weak var profilePic: UIImageView!
     @IBOutlet weak var photoCollectionView: UICollectionView!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var infoTableView: UITableView!
     
-    //This will need to start empty. 
+    //This will need to start empty. TODO: For testing this can stay with just these.
     var photoArray = [UIImage(named: "GardenExample"),UIImage(named: "OvenExample")]
     
     var itemInfo: IndicatorInfo = "Overview"
@@ -31,11 +34,23 @@ class OverviewViewController: UIViewController, IndicatorInfoProvider, UICollect
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        descriptionTextView.layer.cornerRadius = 8
         
+        //CollectionView Delegate and Datasource
         photoCollectionView.delegate = self
         photoCollectionView.dataSource = self
-//        photoArray.append(UIImage(named: "GardenExample")!)
-//        photoArray.append(UIImage(named: "OvenExample")!)
+        
+        //TableView Delege and Datasource
+        infoTableView.delegate = self
+        infoTableView.dataSource = self
+        
+        //Code for changing profile Imageview into a circle.
+        profilePic.layer.borderWidth = 1.0
+        profilePic.layer.masksToBounds = false
+        profilePic.layer.borderColor = UIColor.whiteColor().CGColor
+        profilePic.layer.cornerRadius = profilePic.frame.size.width/2
+        profilePic.clipsToBounds = true
+        
     }
     
     // MARK: - IndicatorInfoProvider
@@ -46,7 +61,7 @@ class OverviewViewController: UIViewController, IndicatorInfoProvider, UICollect
     }
     
     
-    // MARK: CollectionView Data Source Info. This is where the photoArray will be filled up when the view is loaded.
+    // MARK: CollectionView Data Source Info. This is where the photoArray will be filled up when the view is loaded. Firebase Storage will be probably fucked with here. 
     
     
     
@@ -61,9 +76,6 @@ class OverviewViewController: UIViewController, IndicatorInfoProvider, UICollect
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
-        print("lol wtf")
-        
-        //Use a diff cell
         let cell = photoCollectionView.dequeueReusableCellWithReuseIdentifier("overviewVcCell", forIndexPath: indexPath) as! OverviewVcCell
         cell.imageView?.image = self.photoArray[indexPath.row]
         
@@ -71,4 +83,56 @@ class OverviewViewController: UIViewController, IndicatorInfoProvider, UICollect
     }
     
     
+    
+    //MARK: TableView Delegate
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return leftLabelNames.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = infoTableView.dequeueReusableCellWithIdentifier("overviewTableCell") as! OverViewTableCell
+        cell.leftLabel.text = self.leftLabelNames[indexPath.row]
+        cell.rightLabel.text = self.rightLabelInfo[indexPath.row]
+        return cell
+        
+        
+    }
+    
+    //MARK: TableView Data Source
+    var leftLabelNames = ["Start Time", "Hours", "Multiday Job"]
+    
+    //TODO: This will be where the cloud hookup grabs info.
+    var rightLabelInfo = ["9am 12/1/16", "12", "No"]
+    
+    
+    
+    
 }
+    //MARK: Tableview Cell
+class OverViewTableCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var leftLabel: UILabel!
+    @IBOutlet weak var rightLabel: UILabel!
+    
+    override func awakeFromNib() {
+        self.leftLabel.layer.borderWidth = 0.5
+        self.leftLabel.layer.borderColor = UIColor.lightGrayColor().CGColor
+        
+        self.rightLabel.layer.borderWidth = 0.5
+        self.rightLabel.layer.borderColor = UIColor.lightGrayColor().CGColor
+    }
+    
+    
+    
+}
+
+
+
+
